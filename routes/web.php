a18b282fea802f4c7c64c39f658696af2b7a2963
Hossam <hossamsoliuman@gmail.com>
2024-04-27 23:47:07 +0300

    testing branch

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//in testing branch
 Auth::routes([
     'register' => false
 ]);
