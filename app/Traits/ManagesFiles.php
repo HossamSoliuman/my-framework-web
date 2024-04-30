9074f18b4cdd5cdd3adbcdb4a3a7fe8cda890dc7
Hossam <hossamsoliuman@gmail.com>
2024-05-01 01:46:57 +0300

    add move files to trait

--- app/Traits/ManagesFiles.php
+++ app/Traits/ManagesFiles.php
@@ -2,6 +2,7 @@
 
 namespace App\Traits;
 
+use App\Models\TempFile;
 use Illuminate\Support\Facades\File;
 
 trait ManagesFiles
@@ -29,9 +30,21 @@ trait ManagesFiles
      */
     public function deleteFile($filePath)
     {
-
         $file = public_path($filePath);
         $result = File::delete($file);
         return $result;
+    }
+    public function moveTempFile($fileName, $NewfolderName)
+    {
+        $sourcePath = storage_path('app/temp/' . $fileName);
+        $destinationFolder = public_path('files/' . $NewfolderName);
+        if (!file_exists($destinationFolder)) {
+            mkdir($destinationFolder, 0755, true);
+        }
+        $destinationPath = $destinationFolder . '/' . $fileName;
+        rename($sourcePath, $destinationPath);
+        TempFile::where('file_name', $fileName)->delete();
+        $newPath = 'files/' . $NewfolderName . '/' . $fileName;
+        return $newPath;
     }
 }
