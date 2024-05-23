25987d3d8327b04c513eb1cf12c15bf2db8efa05
Hossam <hossamsoliuman@gmail.com>
2024-05-23 18:37:29 +0300

    disable register in api by default

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('login', [AuthenticationController::class, 'login']);
-Route::post('register', [AuthenticationController::class, 'register']);
+//Route::post('register', [AuthenticationController::class, 'register']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::prefix('auth')->group(function () {
         Route::get('user', function (Request $request) {
